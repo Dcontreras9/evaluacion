6ecbcbd6dd81f02495a627fb1492e80538a10a73
David <David@192.168.1.13>
2020-11-22 14:42:41 -0500

    update

---
+++ src/main/java/evaluacion2.java
@@ -0,0 +1,23 @@
+
+import com.sun.java.swing.plaf.windows.WindowsProgressBarUI;
+import java.util.Scanner;
+
+
+public class evaluacion2 {
+
+    public static void main (String [] args){
+        Scanner ingresar = new Scanner(System.in);
+        
+        System.out.println("Ingrese su texto para su edicion");
+        
+        float edicion = ingresar.nextInt();
+        
+        System.out.println(" "+edicion);
+        
+        System.out.println("es correcto lo que va a guardar");
+        
+       
+        }
+        
+}
+}
